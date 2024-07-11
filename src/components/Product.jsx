691695630fd1d06f0dfdc588f6cb56759dd33bd0
neda-ashraf <neda.ashraf246@gmail.com>
2024-07-11 19:10:27 +0530

    Added Carousel component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,16 @@
 import { Typography } from "neetoui";
 
+import Carousel from "./Carousel";
+import { IMAGE_URLS } from "./constants";
+
 const Product = () => (
   <div className="px-6 pb-6">
     <div>
       <Typography
         className="py-2 text-4xl font-semibold"
+        component="blockquote"
         style="h1"
-        weight="black"
+        weight="bold"
       >
         Infinix INBOOK
       </Typography>
@@ -14,35 +18,15 @@ const Product = () => (
     </div>
     <div className="mt-6 flex gap-4">
       <div className="w-2/5">
-        <img
-          alt="Product"
-          className="h-64 w-10/12"
-          src="https://ik.imagekit.io/d9mvewbju/SmileCart/thumbnail_61_7PaLfb.jpg"
-        />
+        <Carousel imageUrls={IMAGE_URLS} title="Infinix Inbook" />
       </div>
       <div className="w-3/5 space-y-4">
-        <Typography style="body3" textTransform="capitalize" weight="medium">
+        <Typography>
           Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey - 1 Year Warranty.
         </Typography>
-        <Typography style="body3" textTransform="capitalize" weight="medium">
-          MRP: $395.97
-        </Typography>
-        <Typography
-          className="font-semibold"
-          style="body3"
-          textTransform="capitalize"
-          weight="medium"
-        >
-          Offer price: $374.43
-        </Typography>
-        <Typography
-          className="font-semibold text-green-600"
-          style="body3"
-          textTransform="capitalize"
-          weight="medium"
-        >
-          6% off
-        </Typography>
+        <Typography>MRP: $395.97</Typography>
+        <Typography className="font-semibold">Offer price: $374.43</Typography>
+        <Typography className="font-semibold text-green-600">6% off</Typography>
       </div>
     </div>
   </div>

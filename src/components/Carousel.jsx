cdd6a28f05bd956e7c158445112eb8f969dd1701
neda-ashraf <neda.ashraf246@gmail.com>
2024-07-12 17:17:32 +0530

    Integrated Ref to the Carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import classNames from "classnames";
 import { Left, Right } from "neetoicons";
@@ -10,16 +10,25 @@ const Carousel = ({ imageUrls, title }) => {
   const handleNext = () =>
     setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
 
-  const handlePrevious = () =>
+  const handlePrevious = () => {
     setCurrentIndex(
       prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
     );
+    resetTimer();
+  };
+
+  const timeRef = useRef(null);
 
   useEffect(() => {
-    const interval = setInterval(handleNext, 3000);
+    timeRef.current = setInterval(handleNext, 3000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(timeRef.current);
   }, []);
+
+  const resetTimer = () => {
+    clearInterval(timeRef.current);
+    timeRef.current = setInterval(handleNext, 3000);
+  };
 
   return (
     <div className="flex flex-col items-center">
@@ -39,7 +48,10 @@ const Carousel = ({ imageUrls, title }) => {
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Right}
           style="text"
-          onClick={handleNext}
+          onClick={() => {
+            handleNext();
+            resetTimer();
+          }}
         />
       </div>
       <div className="flex space-x-1">
@@ -50,7 +62,10 @@ const Carousel = ({ imageUrls, title }) => {
               "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
               { "neeto-ui-bg-black": index === currentIndex }
             )}
-            onClick={() => setCurrentIndex(index)}
+            onClick={() => {
+              setCurrentIndex(index);
+              resetTimer();
+            }}
           />
         ))}
       </div>

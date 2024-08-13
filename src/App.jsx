2d83d7d5f9cdf9b567d718d4901b22dfc3b95dc4
neda-ashraf <neda.ashraf246@gmail.com>
2024-08-13 17:52:04 +0530

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import "./App.css";
-// eslint-disable-next-line import/extensions
+import { NavLink, Route, Switch } from "react-router-dom";
+
+import Home from "./components/Home";
+import PageNotFound from "./components/PageNotFound";
 import Product from "./components/Product";
 
-const App = () => <Product />;
+const App = () => (
+  <>
+    <div className="mx-4 flex space-x-2">
+      <NavLink exact activeClassName="underline font-bold" to="/">
+        Home
+      </NavLink>
+      <NavLink exact activeClassName="underline font-bold" to="/product">
+        Product
+      </NavLink>
+    </div>
+    <Switch>
+      <Route component={Product} path="/product" />
+      <Route component={Home} path="/" />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
+);
 
 export default App;
